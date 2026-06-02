6c291e658685985e29df102052145bfd9e335e9f
Mitesh12ehd <miteshchavda57@gmail.com>
2026-06-02 23:36:18 +0530

    Revert "App: Fix /api/metrics"
    This reverts commit f545ac84e03845b07b528a80b0fe2a64237ac130.

--- application/app/api/metrics/route.ts
+++
@@ -1,9 +0,0 @@
-import { registry } from '@/lib/metrics';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const metrics = await registry.metrics();
-  return new NextResponse(metrics, {
-    headers: { 'Content-Type': registry.contentType },
-  });
-}
